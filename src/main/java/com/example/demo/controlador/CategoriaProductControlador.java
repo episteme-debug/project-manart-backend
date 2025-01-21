7fd53947e97c912e125fe7900f157910ce9461d2
javierR26 <javiecardenas128@gmail.com>
2025-01-20 19:55:13 -0500

    Cambie el post de CategoriaProducto cambie todo la entidad y controlador y servicio para que funcionara con el formdata y ya es funcional

--- src/main/java/com/example/demo/controlador/CategoriaProductControlador.java
+++ src/main/java/com/example/demo/controlador/CategoriaProductControlador.java
@@ -1,14 +1,22 @@
 package com.example.demo.controlador;
 
 import com.example.demo.Entidades.CategoriaProducto;
+import com.example.demo.Entidades.Producto;
+import com.example.demo.Entidades.Usuario;
 import com.example.demo.Repositorios.CategoriaProductoRepositorio;
 import com.example.demo.Servicio.CategoriaProductoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@CrossOrigin("http://127.0.0.1:5500/")
+@CrossOrigin("http://127.0.0.1:5501/")
 public class CategoriaProductControlador {
 
     @Autowired
@@ -17,9 +25,27 @@ public class CategoriaProductControlador {
     @Autowired
     CategoriaProductoRepositorio CategoriaProductoRepositorio;
 
-    @PostMapping("/saveCategoria")
-    public ResponseEntity<CategoriaProducto> saveCategoria(@RequestBody CategoriaProducto categoriaProducto) {
-        CategoriaProducto nuevaCategoria = CategoriaProductoServicio.saveCategoria(categoriaProducto);
-        return ResponseEntity.ok(nuevaCategoria); // Retorna la categoría guardada como JSON
+    @GetMapping( "/GetallCategoria" )
+    public List<CategoriaProducto>GetallCategoria(){
+        return CategoriaProductoServicio.GetallCategoria();
     }
+    @PostMapping(value ="/saveCategoria",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> saveCategoria(
+           @RequestParam("nombreCategoria") String nombreCategoriav,
+           @RequestParam("descripcionCategoria") String descripcionCategoria,
+           @RequestParam("estadoCategoria") boolean estadoCategoria,
+           @RequestParam("imagenCategoria") MultipartFile imagenCategoria,
+           @RequestParam("ordenVisualizacionCategoria") int ordenVisualizacionCategoria
+    ) throws IOException {
+        CategoriaProducto categoriaProducto = new CategoriaProducto();
+        categoriaProducto.setNombreCategoria(nombreCategoriav);
+        categoriaProducto.setDescripcionCategoria(descripcionCategoria);
+        categoriaProducto.setEstadoCategoria(estadoCategoria);
+        categoriaProducto.setImagenCategoria(imagenCategoria.getBytes());
+        categoriaProducto.setOrdenVisualizacionCategoria(ordenVisualizacionCategoria);
+
+        CategoriaProductoRepositorio.save(categoriaProducto);
+
+        return ResponseEntity.ok("se guardo con exito");
+     }
 }
